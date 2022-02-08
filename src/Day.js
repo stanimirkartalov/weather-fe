f84d3197524a65b7feec07b068e606db13b50e00
Stanimir <miro@Stanimirs-MacBook-Pro.local>
2022-02-08 23:17:58 +0200

    change pressure

--- src/Day.js
+++ src/Day.js
@@ -5,7 +5,7 @@ export function Day(props){
         <div className={"day-temperature"}>25C</div>
         <ul>
             <li>rain: 10l/m2</li>
-            <li>pressure: 1012mPa</li>
+            <li>pressure: 1010mPa</li>
             <li>lightness: 70%</li>
             <li>humidity: 30%</li>
         </ul>
